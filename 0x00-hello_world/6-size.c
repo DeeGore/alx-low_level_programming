f5ece7b8187087d691c84532b1aa330dfc746c2a
DeeGore <devinegoredema02@gmail.com>
2023-02-16 10:23:42 -0800

    Size is not a grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char:%lu byte(s)\n"), sizeofchar;
-printf("Size of an int:%lu byte(s)\n"), sizeofint;
-printf("Size of a long int:%lu byte(s)\n"), sizeoflongint;
-printf("Size of a long long int:%lu byte(s)\n"), sizeoflonglongint;
-printf("Size of a float:%lu byte(s)\n"), sizeoffloat;
+printf("Size of a char:%lu byte(s)\n"), size of char;
+printf("Size of an int:%lu byte(s)\n"), size of int;
+printf("Size of a long int:%lu byte(s)\n"), size of longint;
+printf("Size of a long long int:%lu byte(s)\n"), size of long longint;
+printf("Size of a float:%lu byte(s)\n"), size of float;
 return (0);
