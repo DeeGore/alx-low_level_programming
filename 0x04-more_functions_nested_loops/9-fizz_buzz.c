4e7d02d302fc03b400075f06521e1456a696e506
DeeGore <devinegoredema02@gmail.com>
2023-02-23 15:39:08 -0800

    Triangles

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,8 +3,8 @@
 
 /**
  * main - Prints a Fizz Buzz program of numbers 1 to 100
- *
- * Return; 0
+ * Description: Print Fizz for multiples of 3 and Buzz for multiples of 5
+ * Return; Always 0 (Success)
  */
 
 int main(void)
@@ -13,26 +13,26 @@ int main(void)
 
 	for (k = 1; k <= 100; k++)
 	{
-		if ((k % 3 == 0) && (k % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (k % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (k % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", k);
-		}
-		if (k != 100)
-		{
-			printf(" ");
-		}
+	if ((k % 3 == 0) && (k % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (k % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (k % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", k);
+	}
+	if (k != 100)
+	{
+	printf(" ");
+	}
 	}
 	printf("\n");
 	return (0);
