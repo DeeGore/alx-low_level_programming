51429780a116784a48ac04c7295d683b204e929f
DeeGore <devinegoredema02@gmail.com>
2023-03-10 13:35:50 -0800

    Without library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-void _puts(char *s)
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: The string to print
+ * Return: void
+ */
+
+void _puts(char *str)
 {
-	return puts(s);
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
 }
